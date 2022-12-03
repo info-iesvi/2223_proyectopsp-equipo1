a36b2c2039e2ce8392e39896e8c9e1e80f6d7700
Javier Espinar Romero <javikeee.jer@gmail.com>
2022-12-03 11:35:25 +0100

    Recomentando algunas clases

--- src/main/java/appVGShop/gestionUsuarios/infra/controller/EmployeeController.java
+++ src/main/java/appVGShop/gestionUsuarios/infra/controller/EmployeeController.java
@@ -43,8 +43,11 @@ public class EmployeeController {
 
     /**
      * Método para mostrar un empleado según su ID.
+     *
+     * Se toma la id desde la url, se busca con esa id en el repositorio y devuelve un Employee
+     *
      * @param id
-     * @return 404 si no encuentra nada, 200 y el empleado si existe.
+     * @return 404 si no se encuentra nada // 200 si el empleado existe.
      */
     @GetMapping("/employees/{id}")
     public ResponseEntity<?> getUser(@PathVariable Integer id) {
@@ -57,7 +60,10 @@ public class EmployeeController {
     }
 
     /**
-     * Método de guardado de empleado, especificando sus datos en el cuerpo del mensaje.
+     * Método de creación de empleado.
+     *
+     * Se toman los datos del cuerpo, para crear un nuevo empleado.
+     *
      * @param newUserCreator
      * @return 201 con los datos del nuevo usuario
      */
@@ -74,10 +80,14 @@ public class EmployeeController {
 
     /**
      * Método de edición de empleado.
-     * Se introducen los datos de edición en el cuerpo y la ID en la url.
+     *
+     * Se introducen los datos que se van ha editar en el cuerpo y se toma la ID desde la url.
+     * Se realiza un mapeado de los empleados que hay en el repositorio, cuando lo encuentra toma el empleado y
+     * actualiza sus datos.
+     *
      * @param editData
      * @param id
-     * @return 200 si sale bien, 404 si no encuentra el empleado.
+     * @return 200 si se ha podido editar correctamente // 404 si no encuentra el empleado.
      */
     @PutMapping("/employees/{id}")
     public ResponseEntity<?> editUser(@RequestBody EmployeeDTOCreator editData, @PathVariable Integer id) {
@@ -95,7 +105,10 @@ public class EmployeeController {
 
     /**
      * Método de eliminación de empleado.
-     * Recibe la ID del empleado en la url.
+     *
+     * Se recibe la ID del empleado desde la url.
+     * Se toma el repositorio de empleados actual, se buscara y eliminara el empleado con la id que se toma.
+     *
      * @param id
      * @return 204 sin contenido.
      */
