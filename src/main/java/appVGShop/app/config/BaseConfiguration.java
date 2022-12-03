f522d5cadc0ec14961f1e31e87f68c2436febd78
MiguelChaves <mcharod03@gmail.com>
2022-12-03 19:12:53 +0100

    Subido

--- src/main/java/appVGShop/app/config/BaseConfiguration.java
+++ src/main/java/appVGShop/app/config/BaseConfiguration.java
@@ -7,6 +7,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BaseConfiguration {
 
+    /**
+     * ModelMapper es una librería Java para copiar o mapear propiedades de un
+     * tipo de objeto a otro tipo de objeto, permitiendo copiar también los
+     * datos de las referencias a los objetos que contengan.
+     *
+     * @return
+     */
     @Bean
     public ModelMapper modelMapper() {
 
