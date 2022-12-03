c811e87f215946e660dfd9a3661887f66f34bbf9
MiguelChaves <mcharod03@gmail.com>
2022-12-03 20:52:20 +0100

    Subido

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -8,3 +8,6 @@ CREATE TABLE employee (
 );
 
 ALTER TABLE Employee ADD CONSTRAINT FK_EMPLOYEE_ON_EMPLEADOID FOREIGN KEY (codSuperior) REFERENCES Employee (empleadoID);
+
+INSERT INTO employee VALUES (1, 'Juan', 'juan@gmail.com', '1234', 1);
+INSERT INTO employee VALUES (2, 'Ana', 'ana@gmail.com', '5678', 1);
