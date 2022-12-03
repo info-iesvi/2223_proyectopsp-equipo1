324cd250a8d963cd2950739d5b221a4209042fa1
Javier Espinar Romero <javikeee.jer@gmail.com>
2022-12-03 19:35:41 +0100

    Recomentando algunas clases

--- src/main/java/appVGShop/gestionUsuarios/domain/dto/converter/UserDTOConverter.java
+++ src/main/java/appVGShop/gestionUsuarios/domain/dto/converter/UserDTOConverter.java
@@ -12,28 +12,58 @@ public class UserDTOConverter {
 
     private final ModelMapper modelMapper;
 
+    /**
+     * Toma un Employee y lo convierte en un EmployeeDTO
+     *
+     * @param employee
+     * @return
+     */
     public EmployeeDTO fromPropertyToDTO(Employee employee) {
         return modelMapper.map(employee, EmployeeDTO.class);
     }
 
+    /**
+     * Convierte un EmployeeDTOCreator en un Employee
+     *
+     * @param employeeDTOCreator
+     * @return
+     */
     public Employee convertToEmployee (EmployeeDTOCreator employeeDTOCreator) {
 
         return modelMapper.map(employeeDTOCreator, Employee.class);
 
     }
 
+    /**
+     * Convierte un EmployeeDTOUpdate en un Employee
+     *
+     * @param employeeDTOUpdate
+     * @return
+     */
     public Employee convertToEmployee (EmployeeDTOUpdate employeeDTOUpdate) {
 
         return modelMapper.map(employeeDTOUpdate, Employee.class);
 
     }
 
+    /**
+     * Convierte un EmployeeDTODelete en un Employee
+     *
+     * @param employeeDTODelete
+     * @return
+     */
     public Employee convertToEmployee (EmployeeDTODelete employeeDTODelete) {
 
         return modelMapper.map(employeeDTODelete, Employee.class);
 
     }
 
+    /**
+     * Convierte un EmployeeDTOShow en un Employee
+     *
+     * @param employeeDTOShow
+     * @return
+     */
     public Employee convertToEmployee (EmployeeDTOShow employeeDTOShow) {
 
         return modelMapper.map(employeeDTOShow, Employee.class);
